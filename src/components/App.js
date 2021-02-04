e4cf82085cc2d0115ba1b7d94d9183974a0d28eb
P40l051 <73558175+P40l051@users.noreply.github.com>
2021-02-04 16:19:07 +0100

    added beta form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+
+import AddEmployee from './AddEmployee';
 
 class App extends Component {
   render() {
@@ -17,31 +17,7 @@ class App extends Component {
           </a>
         </nav>
         <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Approval Matrix Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
+          <AddEmployee />
         </div>
       </div>
     );
