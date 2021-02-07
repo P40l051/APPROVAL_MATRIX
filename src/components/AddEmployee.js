9e863ce26e93b1656c61adeb47493fe135be0e8a
P40l051 <73558175+P40l051@users.noreply.github.com>
2021-02-07 15:43:19 +0100

    front-end update

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,50 +1,64 @@
 import React, { Component } from 'react'
-import { Card } from 'react-bootstrap';
+import { Accordion, Card, Button } from 'react-bootstrap';
 
 class AddEmployee extends Component {
 
 render() {
     return (
-      <Card>
-      	<div class="container-fluid">
-          <form>
-            <p> </p>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Eth Account</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" e.g.: 0x05B4ca3F7DF84237d8047C24775D406f1157d2Ba" />
-              <small id="emailHelp" class="form-text text-muted">Enter Ethereum adress's employee</small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Employee Name</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: Mario" />
-              <small id="emailHelp" class="form-text text-muted">Enter Employee name</small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: mario@gmail.com" />
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Employee Role</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: foreign affairs" />
-              <small id="emailHelp" class="form-text text-muted">Enter Employee Role</small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Employee Max Baget</label>
-              <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-            </div>
-            <p>
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                <label class="form-check-label" for="exampleCheck1">Check me out if informations are correct.</label>
-              </div>
-            </p>
-            <p>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </p>
-          </form>
-  		  </div>
-      </Card>
+      <div>
+      <Accordion>
+        <Card><Card.Body>Open the following section to add a new employe!</Card.Body></Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              OPEN/COLSE Add Employee!
+            </Accordion.Toggle>
+          </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <div class="container-fluid">
+                  <form>
+                    <p> </p>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Eth Account</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" e.g.: 0x05B4ca3F7DF84237d8047C24775D406f1157d2Ba" />
+                      <small id="emailHelp" class="form-text text-muted">Enter Ethereum adress's employee</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Employee Name</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: Mario" />
+                      <small id="emailHelp" class="form-text text-muted">Enter Employee name</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: mario@gmail.com" />
+                      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Employee Role</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="e.g.: foreign affairs" />
+                      <small id="emailHelp" class="form-text text-muted">Enter Employee Role</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Employee Max Baget</label>
+                      <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <p>
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <label class="form-check-label" for="exampleCheck1">Check me out if informations are correct.</label>
+                      </div>
+                    </p>
+                    <p>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </p>
+                  </form>
+            		</div>
+              </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+      </Accordion>
+      </div>
 	 );
 }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 
 import AddEmployee from './AddEmployee';
-import Collapse from './collapsetest';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Approval Matrix
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <Collapse />
-        </div>
+        <Navbar />
         <div className="container-fluid mt-5">
           <AddEmployee />
         </div>
