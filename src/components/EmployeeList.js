b0f8507e3d6fd5e73ed0dfdf21af7ce50b9af91f
Paolo Tancredi <Paolo.Tancredi@honda-eu.com>
2021-02-16 15:18:56 +0100

    added employee accordion

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
-import { ListGroup, Button } from 'react-bootstrap';
+import { ListGroup, Button, Accordion, Card } from 'react-bootstrap';
 
 class EmployeeProfile extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-            { this.props.employees.map((employee, key) => {
-              return(
-                <div className="card mb-4 mx-auto" key={key}>
-                  <ul className="list-group" >
-                    <li className="list-group-item">ID: {employee.Id}</li>
-                    <li className="list-group-item">Boss Adress: {employee.EmployerBossAdress}</li>
-                    <li className="list-group-item">Employer Adress: {employee.EmployerAdress}</li>
-                    <li className="list-group-item">Name: {employee.EmployeeName}</li>
-                    <li className="list-group-item">Email: {employee.EmployeeEmail}</li>
-                    <li className="list-group-item">Role: {employee.EmployeeRole}</li>
-                    <li className="list-group-item">Division: {employee.EmployeeDivision}</li>
-                    <li className="list-group-item">Location: {employee.EmployeeLocation}</li>
-                    <li className="list-group-item">Power: {employee.EmployeePower}</li>
-                  </ul>
-                </div>
-              )
-            })}
-      </div>
+        <Accordion>
+          <Card><Card.Body>Open the following section to watch registered employees!</Card.Body></Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                OPEN/COLSE Registered Eployees list!
+              </Accordion.Toggle>
+            </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <div className="container-fluid">
+                        { this.props.employees.map((employee, key) => {
+                          return(
+                            <div className="card mb-4 mx-auto" key={key}>
+                              <ul className="list-group" >
+                                <li className="list-group-item">ID: {employee.Id}</li>
+                                <li className="list-group-item">Boss Adress: {employee.EmployerBossAdress}</li>
+                                <li className="list-group-item">Employer Adress: {employee.EmployerAdress}</li>
+                                <li className="list-group-item">Name: {employee.EmployeeName}</li>
+                                <li className="list-group-item">Email: {employee.EmployeeEmail}</li>
+                                <li className="list-group-item">Role: {employee.EmployeeRole}</li>
+                                <li className="list-group-item">Division: {employee.EmployeeDivision}</li>
+                                <li className="list-group-item">Location: {employee.EmployeeLocation}</li>
+                                <li className="list-group-item">Power: {employee.EmployeePower}</li>
+                              </ul>
+                            </div>
+                          )
+                        })}
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+          </Card>
+        </Accordion>
     );
   }
 }
